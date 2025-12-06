187fb9741a2f9d149c8623d9d22afc4bd25d6f87
Oleh Khomei <varyform@gmail.com>
2025-12-06 16:08:57 +0200

    Initial Commit

---
+++ HomekitMonitor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HomekitMonitor
+//
+//  Created by Oleh Khomey on 06.12.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HomekitMonitor/HomekitMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomekitMonitorApp.swift
+//  HomekitMonitor
+//
+//  Created by Oleh Khomey on 06.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct HomekitMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
